138e8ef0c401b3f8f028a52006a0ab61b1b2ee02
Henrique Figueiredo <hft@cesar.school>
2024-11-13 14:38:38 -0300

    minigame basico carai

--- include/characters.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CHARACTERS_H__
-#define __CHARACTERS_H__
-
-void print_mask(int mask_index);
-
-int navigate_masks();
-
-#endif /* __CHARACTERS_H__ */
--- include/map.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MAP_H__
-#define __MAP_H__
-
-void screenDrawMap(int mapIndex);
-
-extern char maps[3][21][55];
-
-#endif /* __MAP_H__ */
